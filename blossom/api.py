309a38ceed63d0ed6e61d0a6bf6d9a63b265caa3
hereariim <herearii.metuarea@gmail.com>
2022-10-20 08:11:15 +0200

    tfv

--- blossom/api.py
+++ blossom/api.py
@@ -67,6 +67,9 @@ import gdown
 import pkg_resources
 from pathlib import Path
 
+import subprocess
+from multiprocessing import Process
+
 BASE_DIR = Path(__file__).resolve().parents[1]
 
 def _catch_error(f):
@@ -77,6 +80,70 @@ def _catch_error(f):
         except Exception as e:
             raise web.HTTPBadRequest(reason=e)
     return wrap
+
+def _fields_to_dict(fields_in):
+    """
+    Function to convert marshmallow fields to dict()
+    """
+    dict_out = {}
+    for k, v in fields_in.items():
+        param = {}
+        param["default"] = v.missing
+        param["type"] = type(v.missing)
+        param["required"] = getattr(v, "required", False)
+
+        v_help = v.metadata["description"]
+        if "enum" in v.metadata.keys():
+            v_help = f"{v_help}. Choices: {v.metadata['enum']}"
+        param["help"] = v_help
+
+        dict_out[k] = param
+
+    return dict_out
+
+
+def mount_nextcloud(frompath, topath):
+    """
+    Mount a NextCloud folder in your local machine or viceversa.
+    Example of usage:
+        mount_nextcloud('rshare:/data/images', 'my_local_image_path')
+    Parameters
+    ==========
+    * frompath: str, pathlib.Path
+        Source folder to be copied
+    * topath: str, pathlib.Path
+        Destination folder
+    """
+    command = ["rclone", "copy", f"{frompath}", f"{topath}"]
+    result = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = result.communicate()
+    if error:
+        warnings.warn(f"Error while mounting NextCloud: {error}")
+    return output, error
+
+
+def launch_cmd(logdir, port):
+    subprocess.call(["tensorboard",
+                     "--logdir", f"{logdir}",
+                     "--port", f"{port}",
+                     "--host", "0.0.0.0"])
+
+
+def launch_tensorboard(logdir, port=6006):
+    """
+    Run Tensorboard on a separate Process on behalf of the user
+    Parameters
+    ==========
+    * logdir: str, pathlib.Path
+        Folder path to tensorboard logs.
+    * port: int
+        Port to use for the monitoring webserver.
+    """
+    subprocess.run(
+        ["fuser", "-k", f"{port}/tcp"]  # kill any previous process in that port
+    )
+    p = Process(target=launch_cmd, args=(logdir, port), daemon=True)
+    p.start()
 
 def conv2d_block(input_tensor, n_filters, kernel_size=3):
     # first layer

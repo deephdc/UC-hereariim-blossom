49dcfc90d0015a5469d20fc44a6b153bf5aa2cc2
hereariim <herearii.metuarea@gmail.com>
2022-10-24 08:43:12 +0200

    xwcwxc

--- blossom/config.py
+++ blossom/config.py
@@ -6,7 +6,6 @@
 import os
 from webargs import fields
 from marshmallow import Schema, INCLUDE
-import yaml
 
 # identify basedir for the package
 BASE_DIR = os.path.dirname(os.path.normpath(os.path.dirname(__file__)))
@@ -29,55 +28,6 @@ MODELS_DIR = os.path.join(IN_OUT_BASE_DIR, 'models')
 DATA_IMAGE = os.path.join(DATA_DIR,os.path.join('train','images'))
 DATA_MASK = os.path.join(DATA_DIR,os.path.join('train','masks'))
 
-# homedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# conf_path = os.path.join(homedir, 'etc', 'config.yaml')
-# with open(conf_path, 'r') as f:
-#     CONF = yaml.safe_load(f)
-
-# def get_conf_dict(conf=CONF):
-#     """
-#     Return configuration as dict
-#     """
-#     conf_d = {}
-#     for group, val in conf.items():
-#         conf_d[group] = {}
-#         for g_key, g_val in val.items():
-#             conf_d[group][g_key] = g_val['value']
-#     return conf_d
-
-
-# conf_dict = get_conf_dict()
-
-# def print_full_conf(conf=CONF):
-#     """
-#     Print all configuration parameters (including help, range, choices, ...)
-#     """
-#     for group, val in sorted(conf.items()):
-#         print('=' * 75)
-#         print('{}'.format(group))
-#         print('=' * 75)
-#         for g_key, g_val in sorted(val.items()):
-#             print('{}'.format(g_key))
-#             for gg_key, gg_val in g_val.items():
-#                 print('{}{}'.format(' '*4, gg_key))
-#                 body = '\n'.join(['\n'.join(textwrap.wrap(line, width=110, break_long_words=False,
-#                                                           replace_whitespace=False,
-#                                                           initial_indent=' '*8, subsequent_indent=' '*8))
-#                                   for line in str(gg_val).splitlines() if line.strip() != ''])
-#                 print(body)
-#             print('\n')
-
-
-# def print_conf_table(conf=conf_dict):
-#     """
-#     Print configuration parameters in a table
-#     """
-#     print("{:<25}{:<30}{:<30}".format('group', 'key', 'value'))
-#     print('=' * 75)
-#     for group, val in sorted(conf.items()):
-#         for g_key, g_val in sorted(val.items()):
-#             print("{:<25}{:<30}{:<15} \n".format(group, g_key, str(g_val)))
-#         print('-' * 75 + '\n')
 
 # Input parameters for predict() (deepaas>=1.0.0)
 class PredictArgsSchema(Schema):
